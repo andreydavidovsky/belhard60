5dd7a7210e0c5f6b77d346947228beec94e26fbd
andreydavidovsky <avtoaviant@gmail.com>
2022-09-12 15:43:35 +0300

    add sp

--- 3_4.py
+++ 3_4.py
@@ -1,9 +1,13 @@
 #Пользователь вводит 3 числа, сказать сколько из них положительных
 #и сколько отрицательных#
-first = int(input())
-second = int(input())
-third = int(input())
-pol = 0
-otr = 0
-
-print(pol, "положительных", otr, "отрицательных")
+#a = int(input())
+#b = int(input())
+# = int(input())
+#some_list = [a, b, c]
+#some_list.count('-')
+#some_list.append('-')
+#print(some_list)
+#s = "5007175"
+#print(s.replace(s[2:], '***'))
+L = list(123,'spam',1.23,'tfdr')
+print(len(L))
